d12780378a4e380f8d24a495ccd5493e9aea4e37
Amit Patel <redblobgames@gmail.com>
2017-09-01 17:09:46 -0700

    Mapgen2 UI

---
+++ mapgen2.js
@@ -0,0 +1,3 @@
+// From http://www.redblobgames.com/maps/mapgen2/
+// Copyright 2017 Red Blob Games <redblobgames@gmail.com>
+// License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
