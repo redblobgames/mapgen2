12091cbbae446b0af5c4d46d3c928cb64bf4c9af
Amit Patel <redblobgames@gmail.com>
2017-09-02 20:28:23 -0700

    Split options across constructor and calculation
    - noisy edges are created once, in the constructor
    - all other data is created multiple times, so options are passed in
      to the calculate() function instead of to the constructor (there's
      no need to save them across invocations)

--- index.js
+++ index.js
@@ -13,8 +13,6 @@
  * limitations under the License.
  */
 
-const SEED = 123456789;
-
 const util =         require('./util');
 const Water =        require('./water');
 const Elevation =    require('./elevation');
@@ -29,41 +27,44 @@ const NoisyEdges =   require('./noisy-edges');
  * Map coordinates are 0 ≤ x ≤ 1000, 0 ≤ y ≤ 1000.
  *
  * mesh: DualMesh
- * noise: function(nx, ny) -> a number from -1 to +1
+ * noisyEdgeOptions: {length, amplitude, seed}
  * makeRandInt: function(seed) -> function(N) -> an int from 0 to N-1
- * options: see code
  */
 class Map {
-    constructor(mesh, noise, makeRandInt, options) {
+    constructor(mesh, noisyEdgeOptions, makeRandInt) {
         this.mesh = mesh;
-        this.noise = noise;
         this.makeRandInt = makeRandInt;
-        this.options = {
-            seed: SEED,
-            shape: {round: 0.5, inflate: 0.4},
-            numRivers: 30,
-            drainageSeed: SEED,
-            riverSeed: SEED,
-            noisyEdge: {length: 10, amplitude: 0.2, seed: SEED},
-            biomeBias: {temperature: 0, moisture: 0},
-        };
-        Object.assign(this.options, options);
-        this.s_lines = NoisyEdges.assign_s_segments(this.mesh, this.options.noisyEdge, this.makeRandInt(this.options.noisyEdge.seed));
+        this.s_lines = NoisyEdges.assign_s_segments(
+            this.mesh,
+            noisyEdgeOptions,
+            this.makeRandInt(noisyEdgeOptions.seed)
+        );
     }
 
-    calculate() {
-        this.r_water = Water.assign_r_water(this.mesh, this.noise, this.options.shape);
+    calculate(options) {
+        console.log('calculate', this.mesh.r_vertex.length);
+        options = Object.assign({
+            noise: null, // required: function(nx, ny) -> number from -1 to +1
+            shape: {round: 0.5, inflate: 0.4},
+            numRivers: 30,
+            drainageSeed: 0,
+            riverSeed: 0,
+            noisyEdge: {length: 10, amplitude: 0.2, seed: 0},
+            biomeBias: {temperature: 0, moisture: 0},
+        }, options);
+
+        this.r_water = Water.assign_r_water(this.mesh, options.noise, options.shape);
         this.r_ocean = Water.assign_r_ocean(this.mesh, this.r_water);
-        this.elevationdata = Elevation.assign_t_elevation(this.mesh, this.r_ocean, this.r_water, this.makeRandInt(this.options.drainageSeed));
+        this.elevationdata = Elevation.assign_t_elevation(this.mesh, this.r_ocean, this.r_water, this.makeRandInt(options.drainageSeed));
         this.t_coastdistance = this.elevationdata.t_distance;
         this.t_elevation = this.elevationdata.t_elevation;
         this.t_downslope_s = this.elevationdata.t_downslope_s;
         this.r_elevation = Elevation.assign_r_elevation(this.mesh, this.t_elevation, this.r_ocean);
-        this.spring_t = util.randomShuffle(Rivers.find_spring_t(this.mesh, this.r_water, this.t_elevation, this.t_downslope_s), this.makeRandInt(this.options.riverSeed));
-        this.river_t = this.spring_t.slice(0, this.options.numRivers);
+        this.spring_t = util.randomShuffle(Rivers.find_spring_t(this.mesh, this.r_water, this.t_elevation, this.t_downslope_s), this.makeRandInt(options.riverSeed));
+        this.river_t = this.spring_t.slice(0, options.numRivers);
         this.s_flow = Rivers.assign_s_flow(this.mesh, this.t_downslope_s, this.river_t, this.t_elevation);
         this.r_moisture = Moisture.assign_r_moisture(this.mesh, this.r_water, Moisture.find_moisture_seeds_r(this.mesh, this.s_flow, this.r_ocean, this.r_water));
-        this.r_biome = Biomes.assign_r_biome(this.mesh, this.r_ocean, this.r_water, this.r_elevation, this.r_moisture, this.options.biomeBias);
+        this.r_biome = Biomes.assign_r_biome(this.mesh, this.r_ocean, this.r_water, this.r_elevation, this.r_moisture, options.biomeBias);
     }
 }
 
